79481301ee94a59bf4bc77e738ea7b39ea9dfd3b
Uche Chris <chrisuche58@yahoo.com>
2023-07-05 20:53:11 +0100

    contact page form

--- pages/components/homepage/Contact.js
+++ pages/components/homepage/Contact.js
@@ -19,7 +19,7 @@ const Contact = () => {
   return (
     <div className='xl:mt-12 xl:flex-row flex-col-reverse flex gap-10 overflow-hidden'>
        <m.div
-       className='flex-[0.75] bg-black p-8 rounded-2xl'
+       className='flex-[0.75] bg-slate-400 p-8 rounded-2xl'
         >
             <p className='text-white font-serif'>Get in touch</p>
             <h3 className='text-white font-extrabold text-3xl'>Contact.</h3>
@@ -38,38 +38,50 @@ const Contact = () => {
                         name='name'
                         value={form.name}
                         onChange={handleChange}
-                        placeholder='What is your Name'
+                        placeholder='What is your Name?'
                         className='bg-teal-800 py-4 px-6 text-white rounded-lg outline-none border-none font-medium placeholder:text-white'
                     />
                 </label>
                 <label className='flex flex-col'>
                     <span className='text-white font-medium mb-4'>
-                        Your Name
+                        Your Email
                     </span>
                     <input
-                        type='text'
-                        name='name'
-                        value={form.name}
+                        type='email'
+                        name='email'
+                        value={form.email}
                         onChange={handleChange}
-                        placeholder='What is your Name'
+                        placeholder="What's your Email?"
                         className='bg-teal-800 py-4 px-6 text-white rounded-lg outline-none border-none font-medium placeholder:text-white'
                     />
                 </label>
                 <label className='flex flex-col'>
                     <span className='text-white font-medium mb-4'>
-                        Your Name
+                        Your mMssage
                     </span>
-                    <input
-                        type='text'
-                        name='name'
-                        value={form.name}
+                    <textarea
+                        rows='7'
+                        name='message'
+                        value={form.message}
                         onChange={handleChange}
-                        placeholder='What is your Name'
+                        placeholder='What do you want to say?'
                         className='bg-teal-800 py-4 px-6 text-white rounded-lg outline-none border-none font-medium placeholder:text-white'
                     />
                 </label>
 
+                <button
+                    type='submit'
+                    className='text-white py-3 px-8 bg-slate-500 font-bold outline-none shadow-md shadow-black w-fit rounded-xl'
+                >
+                    {loading ? 'Sending...' : 'Send'}
+                </button>
+
             </form>
+       </m.div>
+       <m.div
+        className='xl:flex-1 xl:h-auto md:h-[550px] h-[350px]'
+       >
+        
        </m.div>
     </div>
   )
