43fd2eb58401eac607d7ddbd8efd67c690f96abf
Uche Chris <chrisuche58@yahoo.com>
2023-07-27 16:55:41 +0100

    reset

--- pages/components/homepage/MockImage.js
+++ pages/components/homepage/MockImage.js
@@ -1,37 +1,37 @@
-import deved from "/public/dev-ed-wave.png";
-import about from "/public/about.png";
+// import deved from "/public/dev-ed-wave.png";
+// import about from "/public/about.png";
 
-const data = [
-  {
-    id: 1,
-    name: 'Home',
-    img: deved,
-    link: "/"
-  },
-  {
-    id: 2,
-    name: 'Work',
-    img: deved,
-    link: "/Project"
-  },
-  {
-    id: 3,
-    name: 'About',
-    img: about,
-    link: "/About"
-  },
-  {
-    id: 4,
-    name: 'Contact',
-    img: deved,
-    link: "/Contact"
-  },
-  {
-    id: 5,
-    name: 'Experience',
-    img: deved,
-    link: "https://3dplane-chrisuche.vercel.app/"
-  },
-];
+// const data = [
+//   {
+//     id: 1,
+//     name: 'Home',
+//     img: deved,
+//     link: "/"
+//   },
+//   {
+//     id: 2,
+//     name: 'Work',
+//     img: deved,
+//     link: "/Project"
+//   },
+//   {
+//     id: 3,
+//     name: 'About',
+//     img: about,
+//     link: "/About"
+//   },
+//   {
+//     id: 4,
+//     name: 'Contact',
+//     img: deved,
+//     link: "/Contact"
+//   },
+//   {
+//     id: 5,
+//     name: 'Experience',
+//     img: deved,
+//     link: "https://3dplane-chrisuche.vercel.app/"
+//   },
+// ];
 
-export default data;
+// export default data;
