babd0c94088b275d1efb65503e00530692b36737
Uche Chris <chrisuche58@yahoo.com>
2023-07-27 14:16:13 +0100

    build update 2

--- pages/toggle/toggleNav.js
+++ pages/toggle/toggleNav.js
@@ -2,6 +2,20 @@
 //     document.body.dataset.nav = document.body.dataset.nav === "true" ? "false" : "true";
 // }
 
+import React from 'react';
+
+const ToggleNav = () => {
+  const handleToggleNav = () => {
+    document.body.dataset.nav = document.body.dataset.nav === "true" ? "false" : "true";
+  };
+
+  return (
+    <button onClick={handleToggleNav}>Toggle Nav</button>
+  );
+};
+
+export default ToggleNav;
+
 //  const toggleNav = () => {
 //   const body = document.body;
 //   const currentNavState = body.getAttribute("data-nav") === "true";
