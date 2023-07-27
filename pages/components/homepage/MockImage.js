3de2726d235fc02f35fc3c1dca53bf9fa6c04128
Uche Chris <chrisuche58@yahoo.com>
2023-07-27 16:57:30 +0100

    reset 1

--- pages/components/homepage/MockImage.js
+++ pages/components/homepage/MockImage.js
@@ -35,3 +35,24 @@
 // ];
 
 // export default data;
+
+const dsds = (direction, type, delay, duration) => {
+  return {
+    hidden: {
+      x: direction === "left" ? "-100%" : direction === "right" ? "100%" : 0,
+      y: direction === "up" ? "100%" : direction === "down" ? "100%" : 0,
+    },
+    show: {
+      x: 0,
+      y: 0,
+      transition: {
+        type: type,
+        delay: delay,
+        duration: duration,
+        ease: "easeOut",
+      },
+    },
+  };
+};
+
+export default dsds;
